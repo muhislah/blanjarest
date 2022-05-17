8f8fac55a371bc4e558636e058063419b09f149e
Muhamad Islahuddin <muhislah>
2022-05-17 07:29:35 +0700

    deploy2

--- src/config/db.js
+++ src/config/db.js
@@ -7,7 +7,7 @@ const pool = new Pool({
   // database: process.env.DB_DATABASE,
   // password: process.env.DB_PASSWORD,
   // port: process.env.DB_PORT,
-  ssl : true
+  ssl: { rejectUnauthorized : false }
 })
 
 module.exports = pool
