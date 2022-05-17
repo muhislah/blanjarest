a109897f7371719b4ac2187ee2171ec5fc52d5a3
Muhamad Islahuddin <muhislah>
2022-05-17 07:12:56 +0700

    heroku final

--- src/config/db.js
+++ src/config/db.js
@@ -4,7 +4,8 @@ const pool = new Pool({
   host: process.env.DB_HOST,
   database: process.env.DB_DATABASE,
   password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT
+  port: process.env.DB_PORT,
+  ssl : true
 })
 
 module.exports = pool
